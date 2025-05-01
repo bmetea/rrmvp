532c7080ce066995d0c34752f906932e95492aab
Bogdan Metea <bogdan.c.metea@gmail.com>
2025-05-01 23:05:26 +0100

    fix about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,8 @@
-import Navbar from '../components/navigation/Navbar'
-import AnnouncementBanner from '../components/sections/AnnouncementBanner'
 import AboutSection from '../components/sections/AboutSection'
-import Footer from '../components/navigation/Footer'
 
 export default function AboutPage() {
   return (
     <main>
-      <Navbar activePath="/about" />
-      <AnnouncementBanner />
       <div className="pt-16">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center mb-8">
           <h1 className="text-4xl font-bold">About Radiance Rewards</h1>
@@ -15,7 +10,6 @@ export default function AboutPage() {
         </div>
       </div>
       <AboutSection />
-      <Footer />
     </main>
   )
 } 
