a42963e37400055a7bce0e456cc4ebd2c9f6134b
Bogdan Metea <bogdan.c.metea@gmail.com>
2025-05-03 17:14:51 +0100

    sst

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Hero from './components/sections/Hero'
 import WinPrizes from './components/sections/WinPrizes'
 import HowItWorks from './components/sections/HowItWorks'
 import AboutSection from './components/sections/AboutSection'
-import Footer from './components/navigation/Footer'
 
 export default function Home() {
   return (
