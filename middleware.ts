02ba35bc1b236d97f1600df360e5d9cd470eefef
Bogdan Metea <bogdan.c.metea@gmail.com>
2025-05-03 13:27:43 +0100

    add middleware

---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const basicAuth = request.headers.get('authorization')
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1]
+    const [user, pwd] = atob(authValue).split(':')
+
+    // Replace these with your desired username and password
+    if (user === 'admin' && pwd === 'Zubenelgenubi') {
+      return NextResponse.next()
+    }
+  }
+
+  return new NextResponse('Authentication required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  })
+}
+
+export const config = {
+  matcher: '/:path*',
+} 
