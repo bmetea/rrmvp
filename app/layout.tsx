4bba79632f6fc52a61c527f02802f77227bf6573
Bogdan Metea <bogdan.c.metea@gmail.com>
2025-05-19 00:39:58 +0100

    fixed navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,7 +50,6 @@ export const metadata: Metadata = {
   },
 };
 
-
 export default async function RootLayout({
   children,
 }: Readonly<{
@@ -89,7 +88,7 @@ export default async function RootLayout({
               <CartProvider>
                 <div className="flex min-h-screen flex-col">
                   <Navbar />
-                  <main className="flex-1">{children}</main>
+                  <main className="flex-1 pt-16">{children}</main>
                   <Footer />
                 </div>
               </CartProvider>
