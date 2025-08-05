8eb7d1da60624a2255716da59396bbb59f05bad0
Bogdan Metea <bogdan.c.metea@gmail.com>
2025-08-05 23:37:10 +0300

    cancer research banner

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,5 +1,5 @@
 // UI component exports
-export * from "./ui";
+// Individual UI components are imported directly
 
 // Layout component exports
 export { default as Navbar } from "./layout/navigation/Navbar";
@@ -12,13 +12,12 @@ export { default as PageViewTracker } from "./analytics/PageViewTracker";
 export { default as MetaPixel } from "./analytics/MetaPixel";
 export { default as MetaPixelPageTracker } from "./analytics/MetaPixelPageTracker";
 
-
-
 // Section component exports
 export { default as AboutSection } from "./sections/AboutSection";
 export { default as AnnouncementBanner } from "./sections/AnnouncementBanner";
 export { CompetitionList } from "./sections/CompetitionList";
-export { default as FaqSection } from "./sections/FaqSection";
+export { FaqSection } from "./sections/FaqSection";
 export { default as Hero } from "./sections/Hero";
 export { default as HeroCarousel } from "./sections/HeroCarousel";
 export { default as HowItWorks } from "./sections/HowItWorks";
+export { default as SponsorshipBanner } from "./sections/SponsorshipBanner";
